d1aac9ce3700c5ff214b35fbbfbc14b070854a1b
OptAccount <100947704+OptAccount@users.noreply.github.com>
2022-03-04 17:20:00 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='opt',
+version='0.1',
+description='Opt config test',
+url='#',
+author='OverFitter',
+author_email='kozak.bor@gmail.com',
+license='MIT',
+packages=['opt'],
+zip_safe=False)
